a29cd22881169a7f37ef6768422ad212869dc898
Restyled.io <commits@restyled.io>
2021-01-01 21:46:54 +0000

    Restyled by shfmt

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
-	ROOT=$(dirname "$(dirname "$(realpath "$0")")")
+  realpath() { [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"; }
+  ROOT=$(dirname "$(dirname "$(realpath "$0")")")
 else
-	ROOT=$(dirname "$(dirname "$(readlink -f "$0")")")
+  ROOT=$(dirname "$(dirname "$(readlink -f "$0")")")
 fi
 
 cd "$ROOT"
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	NAME=`node -p "require('./product.json').nameLong"`
-	CODE="./.build/electron/$NAME.app/Contents/MacOS/Electron"
+  NAME=$(node -p "require('./product.json').nameLong")
+  CODE="./.build/electron/$NAME.app/Contents/MacOS/Electron"
 else
-	NAME=`node -p "require('./product.json').applicationName"`
-	CODE=".build/electron/$NAME"
+  NAME=$(node -p "require('./product.json').applicationName")
+  CODE=".build/electron/$NAME"
 fi
 
 # Node modules
@@ -26,13 +25,14 @@ yarn electron
 
 # Unit Tests
 if [[ "$OSTYPE" == "darwin"* ]]; then
-	cd "$ROOT" ; ulimit -n 4096 ; \
-		ELECTRON_ENABLE_LOGGING=1 \
-		"$CODE" \
-		test/unit/electron/index.js "$@"
+  cd "$ROOT"
+  ulimit -n 4096
+  ELECTRON_ENABLE_LOGGING=1 \
+    "$CODE" \
+    test/unit/electron/index.js "$@"
 else
-	cd "$ROOT" ; \
-		ELECTRON_ENABLE_LOGGING=1 \
-		"$CODE" \
-		test/unit/electron/index.js --no-sandbox --disable-dev-shm-usage "$@" # Electron 6 introduces a chrome-sandbox that requires root to run. This can fail. Disable sandbox via --no-sandbox.
+  cd "$ROOT"
+  ELECTRON_ENABLE_LOGGING=1 \
+    "$CODE" \
+    test/unit/electron/index.js --no-sandbox --disable-dev-shm-usage "$@" # Electron 6 introduces a chrome-sandbox that requires root to run. This can fail. Disable sandbox via --no-sandbox.
 fi
